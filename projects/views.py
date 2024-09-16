7b8f11923f748438561e31f7fb17367491bb6de3
Andrii0302 <144677052+Andrii0302@users.noreply.github.com>
2024-09-16 18:31:45 +0300

    added user login, logout and flash messages. Added user registration

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
-
+from django.contrib.auth.decorators import login_required
 def projects(request):
     projects=Project.objects.all()
     context={
@@ -13,6 +13,7 @@ def project(request,pk):
     projectObj=Project.objects.get(id=pk)
     #tags = projectObj.tags.all()
     return render(request,'projects/single-project.html',{'project':projectObj})
+@login_required(login_url='login')
 def createProject(request):
     form = ProjectForm()
     if request.method == 'POST':
@@ -23,6 +24,7 @@ def createProject(request):
     context = {'form': form}
     
     return render(request, "projects/project_form.html", context)
+@login_required(login_url='login')
 def updateProject(request,pk):
     project=Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -34,6 +36,7 @@ def updateProject(request,pk):
     context = {'form': form}
     
     return render(request, "projects/project_form.html", context)
+@login_required(login_url='login')
 def deleteProject(request,pk):
     project=Project.objects.get(id=pk)
     if request.method == 'POST':
--- users/views.py
+++ users/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate,logout 
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.models import User
+from .forms import CustomCreationForm
+def loginUser(request):
+    page='login'
+    if request.user.is_authenticated:
+        return redirect('profiles')
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,'Username does not exists')
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('profiles')
+        else:
+            messages.error(request,'Username or password is incorrect ')
+    return render(request,'users/login_register.html')
+def logoutUser(request):
+    logout(request)
+    messages.success(request,'User was logged out')
+    return redirect('login')
+def registerUser(request):
+    page='register'
+    form = CustomCreationForm()
+    #context={'page':page,'form':form}
+    if request.method == 'POST':
+        form=CustomCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request,'User account was created!')
+            login(request,user)
+            return redirect('profiles')
+        else:
+            messages.error(request,'An error has occurred during registration')
+    context={'page':page,'form':form}
+    return render(request,'users/login_register.html',context)
 def profiles(request):
     profiles=Profile.objects.all()
     context={'profiles':profiles}
@@ -10,3 +54,4 @@ def userProfile(request,pk):
     otherSkills=profile.skill_set.filter(description="")
     context={'profile':profile,'topSkills':topSkills,'otherSkills':otherSkills}
     return render(request,'users/user-profile.html',context)
+
