c42df96e782a4b85e9b84e0e44476ca8634a82ff
Andrii0302 <144677052+Andrii0302@users.noreply.github.com>
2024-09-17 11:35:54 +0300

    added user account page to modify their profile

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/',views.registerUser,name='register'),
     path('',views.profiles,name='profiles'),
     path('profile/<str:pk>',views.userProfile,name='user-profile'),
+    path('account/',views.userAccount,name='account'),
 ]
--- users/views.py
+++ users/views.py
@@ -55,3 +55,10 @@ def userProfile(request,pk):
     context={'profile':profile,'topSkills':topSkills,'otherSkills':otherSkills}
     return render(request,'users/user-profile.html',context)
 
+@login_required(login_url='login')
+def userAccount(request):
+    profile=request.user.profile
+    skills=profile.skill_set.all()
+    projects=profile.project_set.all()
+    context={'profile':profile,'skills':skills,'projects':projects}
+    return render(request,'users/account.html',context)
