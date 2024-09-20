0130f85b74a63b82e66e118a0bc80d0c373efdac
Andrii0302 <144677052+Andrii0302@users.noreply.github.com>
2024-09-20 18:33:01 +0300

    auth with jwt, sending post requests

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
@@ -13,7 +15,9 @@ def getRoutes(request):
     ]
     return Response(routes)
 @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
 def getProjects(request):
+    print('USER:', request.user)
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
     return Response(serializer.data)
@@ -22,5 +26,21 @@ def getProjects(request):
 @api_view(['GET'])
 def getProject(request,pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request,pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
     serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
