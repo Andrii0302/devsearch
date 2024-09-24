e74aa24eddb1f27b7820cd215a72bbfbec161a11
Andrii0302 <144677052+Andrii0302@users.noreply.github.com>
2024-09-24 14:46:45 +0300

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-anvc6o%a%70neu!xf810zv=m5sbm^hn)5q8#$)%07)2xa=gfrw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -167,8 +167,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'devsearch.project.andrii@gmail.com'
-EMAIL_HOST_PASSWORD = 'kjlg npvx gusi mwdj'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
